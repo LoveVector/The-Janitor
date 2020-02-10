42f303419c6c113aebff259fdb0879e6b2061b95
Kyle Fernandes <12rmaker@gmail.com>
2020-02-10 13:43:43 +0400

    Worked on movement and FPS camera

---
+++ Assets/Kyle.F/Scripts/CameraLook.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    float rotX;
+    float rotY;
+    public float RotSpeed = 6f;
+    public GameObject camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotate();
+    }
+
+    void CameraRotate()
+    {
+        rotX += Input.GetAxis("Mouse X") * RotSpeed;
+        rotY += Input.GetAxis("Mouse Y") * RotSpeed;
+        rotY = Mathf.Clamp(rotY, -90f, 90f);
+        camera.transform.localRotation = Quaternion.Euler(-rotY, 0f, 0f);
+        transform.rotation = Quaternion.Euler(0f, rotX, 0f);
+    }
+}
---
+++ Assets/Kyle.F/Scripts/Movement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Vector3 movement;
+    public float speed;
+    public float runSpeed;
+    public float walkSpeed;
+    bool running = false;
+    public Transform cameraTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, Input.GetAxis("Vertical") * speed);
+        movement = cameraTransform.TransformDirection(movement);
+        movement.y = 0.0f;
+
+
+        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        {
+            running = true;
+            speed = runSpeed;
+            print("Running");
+        }
+        else
+        {
+            running = false;
+            speed = walkSpeed;
+            print("walking");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movement;
+    }
+}
