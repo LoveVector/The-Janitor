5e73434e858309112081bf600b70fb4cabaeca5d
lilrusty <r.dsouza@saedubai.com>
2020-02-11 12:25:05 +0400

    Temp pistol model added.Crosshair added. Raycast testing.

---
+++ Assets/Raynors Folder/Guns.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Guns : MonoBehaviour
+{
+    public int damage;
+    public float range;
+    public float fireRate;
+    float lastShot;
+
+    public GameObject firePoint;
+    public Camera cam;
+
+    public virtual void Fire()
+    {
+        if (Input.GetMouseButtonDown(0) && Time.time - lastShot >= fireRate)
+        {
+
+        }
+    }
+}
---
+++ Assets/Raynors Folder/Raycast.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    public Camera camera;
+    public GameObject fire;
+    int range = 10;
+
+    Vector3 endPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        endPoint = camera.WorldToViewportPoint(new Vector3(0.5f, 0.5f, 10));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawRay(fire.transform.position, endPoint);
+    }
+}
