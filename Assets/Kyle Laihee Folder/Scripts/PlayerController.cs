cb63dd0b9d1a150a3744d67578d76573e707f9ea
belligerentAlien <superprunejuice@gmail.com>
2020-02-12 23:48:08 +0400

    Took Lesser Kyle's code to test it with my levels, added melee attack, and enemy tag

---
+++ Assets/Kyle Laihee Folder/Scripts/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject hand;
+    public Weapon myWeapon;
+    Animator handAnim;
+
+    float attackTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        handAnim = hand.GetComponentInChildren<Animator>();
+        myWeapon = hand.GetComponentInChildren<Weapon>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AttackMelee();
+    }
+
+    void AttackRayCast()
+    {
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, myWeapon.attackRange))
+        {
+            if (hit.collider.CompareTag("Enemy"))
+            {
+                Debug.Log("EnemyHitWithMelee");
+            }
+        }
+
+    }
+
+    void AttackMelee()
+    {
+        attackTimer += Time.deltaTime;
+        if(Input.GetMouseButtonDown(0) && attackTimer >= myWeapon.attackCoolDown)
+        {
+            handAnim.Play("AttackMeleeHammer");
+        }
+        if(Input.GetMouseButtonUp(0) && attackTimer >= myWeapon.attackCoolDown)
+        {
+            attackTimer = 0f;
+            AttackRayCast();
+        }
+    }
+}
