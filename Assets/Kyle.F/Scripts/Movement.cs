06b9fee464e72ae2e9654272677f557b44453e8d
Kyle Fernandes <12rmaker@gmail.com>
2020-02-18 03:59:37 +0400

    Added Jump, Dash and Sliding. Jump and Sliding still need to be fixed

--- Assets/Kyle.F/Scripts/Movement.cs
+++ Assets/Kyle.F/Scripts/Movement.cs
@@ -5,43 +5,138 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Rigidbody rb;
+
     private Vector3 movement;
+    Vector3 slideDir;
+
     public float speed;
-    public float runSpeed;
-    public float walkSpeed;
-    bool running = false;
-    public Transform cameraTransform;
+    public float dashT;
+    public float dashSp;
+    public float jumpH;
+    public float slideTime;
+
+    bool crouch = false;
+    bool isGrounded;
+    bool sliding = false;
+
+    Animator anim;
+    public Animator collidanim;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        movement = new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, Input.GetAxis("Vertical") * speed);
-        movement = cameraTransform.TransformDirection(movement);
-        movement.y = 0.0f;
-
-
-        if (Input.GetKey(KeyCode.W) && Input.GetKey(KeyCode.LeftShift))
+        if (Input.GetKeyDown(KeyCode.LeftControl))
         {
-            running = true;
-            speed = runSpeed;
-            print("Running");
+            Crouch();
+        }        
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+
+        if (isGrounded = true && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpH, rb.velocity.z);
+            Debug.Log("Jumping");
+        }
+
+        if(Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            StartCoroutine(Dash());  
+        }
+
+        if(Input.GetKey(KeyCode.C))
+        {
+            sliding = true;
+            slideDir = transform.TransformDirection(movement);
+            //crouch = true;
+            slideTime -= Time.deltaTime;
+            //anim.SetBool("isCrouching", true);
+            //collidanim.SetBool("isCrouched", true);
+            rb.velocity = slideDir;
+            if (slideTime <= 0)
+            {
+                sliding = false;
+            }
+            Debug.Log("sliding");
         }
         else
         {
-            running = false;
-            speed = walkSpeed;
-            print("walking");
+            sliding = false;
+            slideTime = 3.0f;
+            //anim.SetBool("isCrouching", false);
+            //collidanim.SetBool("isCrouched", false);
+            //crouch = false;
         }
     }
 
-    private void FixedUpdate()
+    IEnumerator Dash()
     {
+        rb.velocity = movement * dashSp;
+        yield return new WaitForSeconds(dashT);
+        Debug.Log("Dashing");
+    }
+
+    void Move()
+    {
+        movement = Quaternion.Euler(0, rb.transform.eulerAngles.y, 0) * new Vector3(Input.GetAxis("Horizontal") * speed, rb.velocity.y, Input.GetAxis("Vertical") * speed);
         rb.velocity = movement;
+
+        //float h = Input.GetAxis("Horizontal");
+        //float v = Input.GetAxis("Vertical");
+
+        //movement = new Vector3(h, rb.velocity.y, v);
+        //movement = movement.normalized;
+        //movement = transform.TransformDirection(movement);
+
+        //Vector3 pvelocity = movement * speed * Time.deltaTime;
+        //rb.velocity = movement;
+
+        //movement = cameraTransform.TransformDirection(movement);
+        //movement.y = 0.0f;
+    }
+
+    void Crouch()
+    {
+        if (crouch == false)
+        {
+                crouch = true;
+                anim.SetBool("isCrouching", true);
+                collidanim.SetBool("isCrouched", true);
+                float slideForce = 400;
+                rb.AddForce(transform.forward * slideForce);
+                //anim.SetBool("isRunning", false);
+        }
+        else
+        {
+            crouch = false;
+            anim.SetBool("isCrouching", false);
+            collidanim.SetBool("isCrouched", false);
+        }
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = true;
+        }
+    }
+
+    void OnCollisionExit(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
+        }
     }
 }
