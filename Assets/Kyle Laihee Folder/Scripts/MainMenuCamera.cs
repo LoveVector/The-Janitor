162fd85d9f6f1dcf8cdc3b39f6dcdbdeec5a2081
belligerentAlien <superprunejuice@gmail.com>
2020-02-25 10:16:10 +0400

    Worked on the Main Menu scene, created a script for the camera in the main menu

---
+++ Assets/Kyle Laihee Folder/Scripts/MainMenuCamera.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class DefineLerp
+{
+    public static void LerpTransform(this Transform thisObject, Transform thatObject, float t)
+    {
+        thisObject.position = Vector3.Lerp(thisObject.position, thatObject.position, t);
+        thisObject.rotation = Quaternion.Lerp(thisObject.rotation, thatObject.rotation, t);
+        thisObject.localScale = Vector3.Lerp(thisObject.localScale, thatObject.localScale, t);
+    }
+}
+public class MainMenuCamera : MonoBehaviour
+{
+    public Transform[] cameraOptions;
+    public float transitionSpeed;
+    Transform currentView;
+    [SerializeField]
+    int viewIndex = 0;
+
+    void Start()
+    {
+        currentView = cameraOptions[viewIndex];
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+             if(viewIndex > -1)
+             {
+                 currentView = cameraOptions[viewIndex--];
+             }
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            if(viewIndex < cameraOptions.Length + 1)
+            {
+                currentView = cameraOptions[viewIndex++];
+            }
+
+        }
+    }
+
+    void LateUpdate()
+    {
+        transform.LerpTransform(currentView.transform, Time.deltaTime);
+    }
+
+}
